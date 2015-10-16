edcf4081b33d4071707a4f5061574f663043ac7d
Geoffrey Gaillard <ggaillard.cvl@gmail.com>
2015-10-16 22:53:59 +0300

    Added parameters for logic

--- src/swarm/logic.cljs
+++ src/swarm/logic.cljs
@@ -1,23 +1,18 @@
 (ns swarm.logic
-  (:require [swarm.tools :as t]))
-
-
-
-(def min-distance 30)
-
-
+  (:require [swarm.tools :as t])
+  (:require-macros [swarm.macros :refer [def-]]))
 
 
 ;; TOOLS
 
 
 
-(defn distance-vec
+(defn- distance-vec
   "Computi euclidian distance between two js/THREE.Vector3"
   [v1 v2]
   (.distanceTo v1 v2))
 
-(defn distance
+(defn- distance
   "Compute the euclidian distance between two boids"
   [boid1 boid2]
   (distance-vec (.-position boid1) (.-position boid2)))
@@ -25,12 +20,12 @@
 
 
 
-(defn get-closests
+(defn- get-closests
   "Return a sorted list of the n closests neighbours"
   [item others n]
   (let [distances (map (fn [other]
-                                [ (.distanceTo (.-position item) (.-position other))
-                                 other]) others)
+                         [ (.distanceTo (.-position item) (.-position other)) ,
+                           other]) others)
         sorted (sort-by first distances)]
     (clj->js
      (mapv last (take n sorted)))))
@@ -40,7 +35,7 @@
 
 ;; Re-use a unique object to improve perfs
 ;; Acceptable since we are in a single threaded environnement
-(def center-shared-vector (new js/THREE.Vector3))
+(def- center-shared-vector (new js/THREE.Vector3))
 
 (defn- get-center
   "Give the center of the local group"
@@ -54,22 +49,22 @@
 ;; EXTRA
 
 
-(defn limit-speed!
+(defn- limit-speed!
   "Limit the speed of a boid by reducing the velocity"
-  [velocity]
+  [velocity max-speed]
   (let [length (.length velocity)]
-    (if (> length 3)
+    (if (> length max-speed)
       (do
         (-> (.normalize velocity)
-            (.multiplyScalar 3)))
+            (.multiplyScalar max-speed)))
       (do
         velocity))))
 
 
 
-(def center (new js/THREE.Vector3 100 100 100))
+(def- center (new js/THREE.Vector3 100 100 100))
 
-(defn stay-in-space
+(defn- stay-in-space
   "Force tho boids to stay near the center"
   [item]
   (-> (.clone center)
@@ -77,10 +72,10 @@
       (.divideScalar 1000)))
 
 
-(def wind-default (-> (new js/THREE.Vector3  -10 -10 -10)
+(def- wind-default (-> (new js/THREE.Vector3  -10 -10 -10)
                       (.divideScalar 100)))
 
-(defn wind
+(defn- wind
   "Return a wind force"
   []
   wind-default)
@@ -92,23 +87,23 @@
 ;; RULES
 
 
-(defn go-to-center
+(defn- go-to-center
   "Make a boid move to the center of his group one step
   Compute de diff Vector between the boid and the center then add 1/100 of its length to the direction
   for a smooth turn"
   [item center]
   (-> (.clone center)
-        (.sub (.-position item))
-        (.divideScalar 50)))
+      (.sub (.-position item))
+      (.divideScalar 50)))
 
 
 
 
-(def shared-position (new js/THREE.Vector3))
+(def- shared-position (new js/THREE.Vector3))
 
-(defn avoid-collision
+(defn- avoid-collision
   "Avoid collision with other boids"
-  [item others]
+  [item others min-distance]
   (let [pos (.-position item)]
     (.set shared-position 0 0 0)
     (doseq [other others]
@@ -131,7 +126,7 @@
 
 ;; Re-use a unique object to improve perfs
 ;; Acceptable since we are in a single threaded environnement
-(def avg-shared-vector (new js/THREE.Vector3))
+(def- avg-shared-vector (new js/THREE.Vector3))
 
 (defn follow-velocity
   "Set the velocity to the average of the group"
@@ -154,14 +149,14 @@
 
 
 
-(defn set-position!
+(defn- set-position!
   "Change position of an item"
-  [item others]
-  (let [neighbours (get-closests item others 5)
+  [item others min-distance nb-neighbours max-speed]
+  (let [neighbours (get-closests item others nb-neighbours)
         center (get-center neighbours)
 
         v1 (go-to-center item center)
-        v2 (avoid-collision item neighbours)
+        v2 (avoid-collision item neighbours min-distance)
         v3 (follow-velocity item neighbours)
         center (stay-in-space item)
         wind (wind)
@@ -171,11 +166,10 @@
                      (.add v3)
                      (.add center)
                      (.add wind)
-                     (limit-speed!))]
+                     (limit-speed! max-speed))]
 
     ;(set! (.-velocity item) velocity)
-    (.add (.-position item) velocity)
-    ))
+    (.add (.-position item) velocity)))
 
 
 
@@ -183,6 +177,7 @@
 
 (defn update-boids!
   "Use swarm intelligence to make tho boids move in a closed space."
-  [boids]
+  [boids min-distance nb-neighbours max-speed]
   (doseq [item boids]
-    (set-position! item boids)))
+    (set-position! item boids min-distance nb-neighbours max-speed)
+    ))
